ec35eccf76ae4b5a642d98b7ecd73e4d70bb1ebb
Danny Willems <contact@danny-willems.be>
2016-04-27 14:53:15 +0200

    remove different methods with callback and unify them

--- cordova_clipboard.mli
+++ cordova_clipboard.mli
@@ -3,33 +3,24 @@ class clipboard : Ojs.t ->
   object
     inherit Ojs.obj
     (* --------------- COPY --------------- *)
-    (* copy [str] *)
-    method copy         : string -> unit
-    [@@js.call "copy"]
-
-    (* copy [str] [success_cb] *)
-    method copy_succ    : string          ->
-                          (unit -> unit)  ->
-                          unit
-    [@@js.call "copy"]
-
-    (* copy [str] [success_cb] [error_cb] *)
-    method copy_cb      : string            ->
-                          (unit -> unit)    ->
-                          (string -> unit)  ->
-                          unit
+    (* copy [str] ?[success_cb] *)
+    method copy     : string          ->
+                      ?success_cb:((unit -> unit) [@js.default (fun () -> ())])
+                                      ->
+                      ?error_cb:((string -> unit) [@js.default (fun e -> ())])
+                                      ->
+                      unit            ->
+                      unit
     [@@js.call "copy"]
     (* ------------------------------------ *)
 
     (* -------------- PASTE --------------- *)
-    (* paste [success_cb] *)
-    method paste        : (string -> unit) ->
-                          unit
-    [@@js.call "paste"]
-    (* paste_cb [success_cb] [error_cb] *)
-    method paste_cb     : (string -> unit) ->
-                          (string -> unit) ->
-                          unit
+    (* paste_cb [success_cb] ?[error_cb] *)
+    method paste    : (string -> unit) ->
+                      ?error_cb:((string -> unit) [@js.default (fun e -> ())])
+                                      ->
+                      unit            ->
+                      unit
     [@@js.call "paste"]
     (* ------------------------------------ *)
   end
