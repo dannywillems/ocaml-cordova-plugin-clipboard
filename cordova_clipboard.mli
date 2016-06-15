96842af6667409a675effa699a11c54c555febad
Danny Willems <contact@danny-willems.be>
2016-06-15 02:04:12 +0200

    Use functions instead of objects

--- cordova_clipboard.mli
+++ cordova_clipboard.mli
@@ -1,32 +1,20 @@
-(* -------------------------------------------------------------------------- *)
-class clipboard : Ojs.t ->
-  object
-    inherit Ojs.obj
-    (* --------------- COPY --------------- *)
-    (* copy [str] ?[success_cb] *)
-    method copy     : string          ->
-                      ?success_cb:((unit -> unit) [@js.default (fun () -> ())])
-                                      ->
-                      ?error_cb:((string -> unit) [@js.default (fun e -> ())])
-                                      ->
-                      unit            ->
-                      unit
-    [@@js.call "copy"]
-    (* ------------------------------------ *)
+(* --------------- COPY --------------- *)
+(* copy [str] ?[success_cb] *)
+val copy :
+  string                                                    ->
+  ?success_cb:((unit -> unit) [@js.default (fun () -> ())]) ->
+  ?error_cb:((string -> unit) [@js.default (fun e -> ())])  ->
+  unit                                                      ->
+  unit
+[@@js.global "cordova.plugins.copy"]
+(* ------------------------------------ *)
 
-    (* -------------- PASTE --------------- *)
-    (* paste_cb [success_cb] ?[error_cb] *)
-    method paste    : (string -> unit) ->
-                      ?error_cb:((string -> unit) [@js.default (fun e -> ())])
-                                      ->
-                      unit            ->
-                      unit
-    [@@js.call "paste"]
-    (* ------------------------------------ *)
-  end
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> clipboard
-[@@js.get "cordova.plugins.clipboard"]
-(* -------------------------------------------------------------------------- *)
+(* -------------- PASTE --------------- *)
+(* paste [success_cb] ?[error_cb] *)
+val paste :
+  (string -> unit)                                          ->
+  ?error_cb:((string -> unit) [@js.default (fun e -> ())])  ->
+  unit                                                      ->
+  unit
+[@@js.global "cordova.plugins.paste"]
+(* ------------------------------------ *)
